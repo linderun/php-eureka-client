028cc71e72a4287baa02ecf3ebffa9937cf1c50e
darren <darren@rantion.com>
2020-06-29 11:09:19 +0800

    只获取上线状态的实例

--- src/Discovery/RandomStrategy.php
+++ src/Discovery/RandomStrategy.php
@@ -16,6 +16,14 @@ class RandomStrategy implements DiscoveryStrategy
             return null;
         }
 
-        return $instances[rand(0, count($instances) - 1)];
+        // 获取上线状态的实例
+        $onlineInstances = [];
+        foreach ($instances as $_instance) {
+            if (strtoupper($_instance['status']) == 'UP') {
+                $onlineInstances[] = $_instance;
+            }
+        }
+
+        return $onlineInstances[rand(0, count($onlineInstances) - 1)];
     }
 }
